4ab31578f0a432cf87ead76b36d6a1e7a3d13fa3
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-01-18 04:14:30 +0000

    Automatically backed up by Learn

--- ruby lib/my_all.rb
+++ ruby lib/my_all.rb
@@ -4,7 +4,7 @@ my_all?([1,2,3]) {|1| 1 < 2}
   block_returns_values = [true, false, false]
   while i < collection.length
     block_returns_values << yield(collection[i])
-    i = i + 1
+    i = i + 1 #i += 1 does the same thing. use this if it's easierfor you.
   end
 
   if block_returns_values.include?(false)
