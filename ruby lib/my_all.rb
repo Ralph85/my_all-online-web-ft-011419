63a4ac9cf40d33852603c09579751dfd6dc2904c
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-01-18 04:15:29 +0000

    Automatically backed up by Learn

--- ruby lib/my_all.rb
+++ ruby lib/my_all.rb
@@ -3,7 +3,7 @@ my_all?([1,2,3]) {|1| 1 < 2}
   i == 0
   block_returns_values = [true, false, false]
   while i < collection.length
-    block_returns_values << yield(collection[i])
+    block_returns_values << yield(1,2,3[i])
     i = i + 1 #i += 1 does the same thing. use this if it's easierfor you.
   end
 
